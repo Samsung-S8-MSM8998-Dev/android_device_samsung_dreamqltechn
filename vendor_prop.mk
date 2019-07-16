5e37059e4a7c967e214d6c4991a1a4752d129793
Sujin Panicker <spanic@codeaurora.org>
2019-07-16 18:52:05 +0530

    dreamqlte: Fix rank of QC OMX components
    Rank OMX SW codecs higher than C2 codecs except for opus codec.
    
    Change-Id: Id8d760e2fc8d33e8c8c97ed1ee10513c71457aa5
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -75,6 +75,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/persistent
 
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0
+
 # Memory
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m \
