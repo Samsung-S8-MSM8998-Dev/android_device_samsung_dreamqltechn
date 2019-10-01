b812230e9cc7b0d2bb5afab2a20849c3c9c0865d
Michael Bestas <mkbestas@lineageos.org>
2019-10-01 21:14:38 +0300

    dreamqlte: Remove rootdir symlinks and /dsp sepolicy
    * No longer needed with pie blobs
    
    Change-Id: I3495fe5a3475c24c41eaedcb830fb12dfe395e44

--- config.fs
+++ config.fs
@@ -60,21 +60,3 @@ mode: 0771
 user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
-
-[bt_firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[persist/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[dsp/]
-mode: 0771
-user: AID_MEDIA
-group: AID_MEDIA
-caps: 0
