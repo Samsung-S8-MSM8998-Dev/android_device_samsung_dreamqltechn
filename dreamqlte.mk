f7d0d2fb9a22a2eb78a7b4858a6f5a1175885824
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    dreamqlte: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- dreamqlte.mk
+++ dreamqlte.mk
@@ -319,13 +319,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.post_boot.sh
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl
