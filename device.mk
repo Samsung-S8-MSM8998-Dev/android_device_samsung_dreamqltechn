0826c1ea5016368727c09bec3215415c77641b3f
baolong24 <1667375308@qq.com>
2021-06-13 16:32:26 +0800

    dreamqlte: Drop camera
    Signed-off-by: baolong24 <1667375308@qq.com>

--- device.mk
+++ device.mk
@@ -128,17 +128,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
-
-PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
-    android.hardware.camera.device@3.4:64 \
-    android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4:64 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5:64 \
-    vendor.qti.hardware.camera.device@1.0:64
 
 # Display
 PRODUCT_PACKAGES += \
