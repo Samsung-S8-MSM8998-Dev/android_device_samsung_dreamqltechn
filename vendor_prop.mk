5924432c2af6c60ac201a717fb58440ac0d281d2
baolong24 <1667375308@qq.com>
2021-06-22 22:56:17 +0800

    dreamqlte: Update props from G9500ZHS6DUD1
    Signed-off-by: baolong24 <1667375308@qq.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -16,37 +16,30 @@
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    aaudio.hw_burst_min_usec=2000 \
-    aaudio.mmap_exclusive_policy=2 \
-    aaudio.mmap_policy=2 \
-    af.fast_track_multiplier=2 \
+    af.fast_track_multiplier=1 \
     audio.adm.buffering.ms=6 \
     audio.deep_buffer.media=true \
-    audio.offload.min.duration.secs=15 \
     audio.offload.video=true \
-    persist.audio.dirac.speaker=true \
-    persist.audio.speaker.dualmode=true \
     persist.vendor.audio.fluence.speaker=true \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.ras.enabled=false \
     persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
-    ro.config.vc_call_vol_steps=11 \
-    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.fluencetype=none \
     ro.vendor.audio.sdk.ssr=false \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
     vendor.audio.flac.sw.decoder.24bit=true \
-    vendor.audio.hw.aac.encoder=false \
+    vendor.audio.hw.aac.encoder=true \
     vendor.audio.noisy.broadcast.delay=600 \
     vendor.audio.offload.buffer.size.kb=32 \
     vendor.audio.offload.gapless.enabled=true \
     vendor.audio.offload.multiaac.enable=true \
-    vendor.audio.offload.multiple.enabled=true \
+    vendor.audio.offload.multiple.enabled=false \
     vendor.audio.offload.passthrough=false \
     vendor.audio.offload.pstimeout.secs=3 \
-    vendor.audio.offload.track.enable=false \
-    vendor.audio.parser.ip.buffer.size=0 \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.safx.pbe.enabled=true \
     vendor.audio.tunnel.encode=false \
     vendor.audio.use.sw.alac.decoder=true \
@@ -55,22 +48,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    qcom.bluetooth.soc=cherokee \
-    ro.bluetooth.hfp.ver=1.6
+    qcom.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=1 \
-    persist.camera.set.afd=4 \
-    persist.camera.xm.green.b=0.96 \
-    persist.camera.xm.green.r=0.97 \
-    persist.dualcam.lpm.enable=1 \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.miui.cit,org.lineageos.snap \
-    vidc.enc.dcvs.extra-buff-count=2
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.cne.feature=1
+    persist.cne.feature=0
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -79,31 +65,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     ro.opengles.version=196610 \
-    ro.sf.lcd_density=480 \
-    vendor.display.enable_default_color_mode=1
-
-# DPM
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.dpm.feature=1 \
-    persist.vendor.dpm.nsrm.bkg.evt=3955
+    ro.sf.lcd_density=480
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/platform/soc/1da4000.ufshc/by-name/frp
-
-# IMS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.vendor.qti.telephony.vt_cam_interface=1
+    ro.frp.pst=/dev/block/persistent
 
 # Memory
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapmaxfree=8m \
-    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapminfree=2m \
     dalvik.vm.heapsize=512m \
-    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapstartsize=8m \
     dalvik.vm.heaptargetutilization=0.75
 
 # Network manager
@@ -121,52 +95,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.qti.core_ctl_max_cpu=4 \
-    ro.vendor.qti.core_ctl_min_cpu=0 \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
-    sdm.debug.disable_skip_validate=1 \
-    sdm.perf_hint_window=50
+    sdm.debug.disable_skip_validate=1
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.mode=concurrent \
-    persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.dynamic_sar=false \
     persist.radio.multisim.config=dsds \
     persist.rmnet.data.enable=true \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.force_on_dc=true \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.redir_party_num=1 \
-    persist.vendor.radio.report_codec=1 \
     persist.vendor.radio.sib16_support=1 \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ro.telephony.default_cdma_sub=0 \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ro.telephony.default_network=22,22 \
     ro.use_data_netmgrd=true \
     telephony.lteOnCdmaDevice=1 \
     vendor.voice.path.for.pcm.voip=true
-
-# Sensors
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.sensors.cfos=false \
-    persist.sys.sensors.pug=false \
-    persist.sys.sensors.spg=false \
-    ro.qti.sensors.taptap_gesture=true \
-    ro.vendor.sdk.sensors.gestures=false \
-    ro.vendor.sensors.cmc=false \
-    ro.vendor.sensors.dev_ori=false \
-    ro.vendor.sensors.dpc=true \
-    ro.vendor.sensors.facing=false \
-    ro.vendor.sensors.iod=false \
-    ro.vendor.sensors.mot_detect=true \
-    ro.vendor.sensors.multishake=true \
-    ro.vendor.sensors.pmd=false \
-    ro.vendor.sensors.sta_detect=true
-
-# Time services
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.delta_time.enable=true
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
